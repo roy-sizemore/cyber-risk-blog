14024fcbc9e895edca91cac0e542a6a5e159de19
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-11 13:16:26 -0400

    Added API route js files and added comment routes

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
