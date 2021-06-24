56817222e6f63c1ddcde922ba1b72ace09d4b345
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-24 12:04:21 -0400

    Fixed update and delete post bugs

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,10 +1,10 @@
-const postId = document.querySelector('input [name="post-id"]').value;
+const postId = document.querySelector('input[name="post-id"]').value;
 
 const editForm = async function(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input [name="post-title"]').value;
-    const body = document.querySelector('textarea [name="post-body"]').value;
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
 
     await fetch(`/api/post/${postId}`, {
         method: 'PUT',
